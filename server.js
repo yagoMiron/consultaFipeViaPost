0b5ea093820247727e062dc09a649d897a1d9229
Yago Miron <yagomiron.exe@gmail.com>
2025-12-23 00:52:08 -0400

    criando server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const __dirname = new URL(".", import.meta.url).pathname;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// arquivos estáticos do React
+app.use(express.static(path.join(__dirname, "build")));
+
+// Bitrix pode chamar por POST (iframe)
+app.post("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
